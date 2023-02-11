159b9c5e10c8e1d3167ece0c50e0338f018108cd
jimin0601 <jimin600155@naver.com>
2023-02-11 21:27:42 +0900

    jimin 02.11 last commit

--- src/CurrencyInput.js
+++ src/CurrencyInput.js
@@ -70,14 +70,16 @@ const ExchangeStyle = styled.div`
 
 function CurrencyInput() {
   
+  // 초기 값 세팅
   const [amount, setAmount] = useState('1');
-  const [currency1, setCurrency1] = useState("KRW");
-  const [currency2, setCurrency2] = useState("USD");
+  const [currency1, setCurrency1] = useState("USD");
+  const [currency2, setCurrency2] = useState("KRW");
   const [keys, setKeys] = useState([]);
   const [objects, setObjects] = useState({});
   
   const onChnageAmount = e => {
     let value = e.target.value;
+    // 값에 숫자가 아니거나 null이 들어오면 0
     if(isNaN(value) || value === null) {
       setAmount('0');
     } else {
@@ -87,11 +89,15 @@ function CurrencyInput() {
   const onChangeValue1 = e => setCurrency1(e.target.value);
   const onChangeValue2 = e => setCurrency2(e.target.value);
 
+  // 처음 렌더링 후, 금액이나, 통화가 바뀔 때 마다 렌더링
   useEffect(() => {
     const onExchangeRate = async () => {
+      // 서버 연결
       const response = await ExchangeAPI.exchangeRate(currency1, amount);
       if(response.status === 200) {
+        // String으로 받아서 JSON 형식으로 파싱
         const jsonObj = JSON.parse(response.data.obj);
+        // 객체의 키만 가져옴
         const keys = Object.keys(jsonObj);
         setKeys(keys.map((key) => key));
         setObjects(jsonObj);
@@ -110,6 +116,7 @@ function CurrencyInput() {
           <div className="select-box">
             <div className="line"/>
               <select onChange={onChangeValue1} value={currency1}>
+                {/* 받아온 키의 정보를 map을 돌려서 option에 값 넣기 */}
                 {keys.map((key => (
                   <option key={key} value={key}>{key}</option>
                   )))}
--- src/ExchangeAPI.js
+++ src/ExchangeAPI.js
@@ -6,7 +6,7 @@ const HEADER = {'Content-Type' : 'application/json'}
 // 서버 통신
 const ExchangeAPI = {
   exchangeRate : async function(baseCode, amount) {
-    if(amount === '') amount = 1;
+    if(amount === '') amount = 0;
     const obj = {
       baseCode : baseCode,
       amount : amount
